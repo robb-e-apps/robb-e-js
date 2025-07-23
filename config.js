5d8fb942c5b4a4df2cc212f2f2dc2f5e1a46d4fe
shabalin-aleksandr <sasha19822010@gmail.com>
2025-07-23 15:08:54 +0200

    feat: add config constants

---
+++ config.js
@@ -0,0 +1,5 @@
+export const API_BASE = 'http://localhost:3000';
+export const REDIRECT_BASE = 'http://localhost:8080';
+export const REDIRECT_URI = `${REDIRECT_BASE}/api/callback`;
+export const CLIENT_ID = '';
+export const TENANT_CODE = '';
