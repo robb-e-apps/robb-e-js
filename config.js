803db3320f577d82ee8a948d424405ce9e4edb2c
shabalin-aleksandr <sasha19822010@gmail.com>
2025-07-24 16:05:24 +0200

    chore: delete unused const

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ export const HOST_IP = '0.0.0.0';
 export const ROBBE_BE_URL = `${HOST}:${PORT_BE}`;
 export const ROBBE_FE_URL = `${HOST}:${PORT_FE}`;
 
-export const REDIRECT_URI = `${ROBBE_FE_URL}/oauth-callback`;
 export const APPLICATION_CODE = '1f06708c-b44f-6a20-9695-aa8c81cce146';
 
 // Put {HOST}:{PORT}/oauth-callback as Redirect URL in your application in Robb-e
