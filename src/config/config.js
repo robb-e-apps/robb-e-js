0b55715da538ea82f1256b597f51ab5bec9ddad3
Aleksandr Shabalin <sasha19822010@gmail.com>
2026-01-13 10:07:41 +0100

    Feature: Prepare App to communicate with staging (#4)
    * feat: add staging urls to env
    
    * fix: correct url to communicate with be

--- src/config/config.js
+++ src/config/config.js
@@ -7,11 +7,8 @@ if (fs.existsSync('.env.local')) {
 
 dotenv.config();
 
-const PORT_FE = process.env.PORT_FE;
-const PORT_BE = process.env.PORT_BE;
 export const PORT = process.env.PORT;
 export const HOST = process.env.HOST;
 export const APPLICATION_CODE = process.env.APPLICATION_CODE;
-
-export const ROBBE_BE_URL = `${HOST}:${PORT_BE}`;
-export const ROBBE_FE_URL = `${HOST}:${PORT_FE}`;
+export const ROBB_E_BE_URL = process.env.HOST_ROBB_E_BE;
+export const ROBB_E_FE_URL = process.env.HOST_ROBB_E_FE;
--- src/m2m/api/proxy.js
+++ src/m2m/api/proxy.js
@@ -1,7 +1,7 @@
-import { ROBBE_BE_URL } from '../../config/config.js';
+import { ROBB_E_BE_URL } from '../../config/config.js';
 
 export async function forwardTokenRequest(grantType, body) {
-  const url = `${ROBBE_BE_URL}/application/client/oauth/token?grant_type=${grantType}`;
+  const url = `${ROBB_E_BE_URL}/application/client/oauth/token?grant_type=${grantType}`;
 
   const response = await fetch(url, {
     method: 'POST',
